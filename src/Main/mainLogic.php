cd8b3b34b2676ffb53647f37916e44993df2ae88
Atrem <artem.ivanov@sandsiv.com>
2018-05-03 19:25:07 +0300

    works

--- src/Main/mainLogic.php
+++ src/Main/mainLogic.php
@@ -15,16 +15,16 @@ $rectangle1 = new Shapes\Rectangle(22, 16);
 $square1 = new Shapes\Square(8, 8);
 
 
-//$triangle2 = new Shapes\Triangle(9, 12, 15);
-//$sphere2 = new Shapes\Sphere(45);
-//$rectangle2 = new Shapes\Rectangle(29, 26);
-//$square2 = new Shapes\Square(18, 18);
-//
-//
-//$triangle3 = new Shapes\Triangle(9, 12, 15);
-//$sphere3 = new Shapes\Sphere(84);
-//$rectangle3 = new Shapes\Rectangle(32, 16);
-//$square3 = new Shapes\Square(80, 80);
+$triangle2 = new Shapes\Triangle(9, 12, 15);
+$sphere2 = new Shapes\Sphere(45);
+$rectangle2 = new Shapes\Rectangle(29, 26);
+$square2 = new Shapes\Square(18, 18);
+
+
+$triangle3 = new Shapes\Triangle(9, 12, 15);
+$sphere3 = new Shapes\Sphere(84);
+$rectangle3 = new Shapes\Rectangle(32, 16);
+$square3 = new Shapes\Square(80, 80);
 
 
 $container1 = new ShapeContainer();
@@ -33,25 +33,25 @@ $container1->addShape($square1);
 $container1->addShape($sphere1);
 $container1->addShape($rectangle1);
 
+$container2 = new ShapeContainer();
+$container2->addShape($square2);
+$container2->addShape($rectangle2);
+$container2->addShape($sphere2);
+$container2->addShape($triangle2);
 
-foreach ($container1->getShapes() as $value) {
+$container3 = new ShapeContainer();
+$container3->addShape($square3);
+$container3->addShape($rectangle3);
+$container3->addShape($sphere3);
+$container3->addShape($triangle3);
+
+foreach ($container1->getShapes() as $value)
+     {
+        echo $value;
+    }foreach ($container2->getShapes() as $value)
+     {
+        echo $value;
+    }foreach ($container3->getShapes() as $value)
+     {
         echo $value;
     }
-
-
-
-//$container2 = new ShapeContainer();
-//$container2->addShape($square2);
-//$container2->addShape($rectangle2);
-//$container2->addShape($sphere2);
-//$container2->addShape($triangle2);
-//$container2->addShape($container1);
-//
-//$container3 = new ShapeContainer();
-//$container3->addShape($square3);
-//$container3->addShape($rectangle3);
-//$container3->addShape($sphere3);
-//$container3->addShape($triangle3);
-//$container3->addShape($container2);
-
-
